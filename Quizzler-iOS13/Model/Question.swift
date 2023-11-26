ec8343da5b7003e1345ce094bc833e080802c6ea
Christopher Mena <you@chrismena.dev>
2023-11-25 19:18:41 -0500

    Moved project to the MVC pattern

--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+struct Question {
+    var text: String
+    var answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+}
